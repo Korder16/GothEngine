e2a6fa6d14f32605bd48667bbecb7ad60ad12a48
Islam <islam_tramov@mail.ru>
2022-07-31 21:30:54 +0300

    Separate main()

--- engine/main.cpp
+++ engine/main.cpp
@@ -1,10 +1,4 @@
-#include <iostream>
-#include "../math/vec2.hpp"
-
 int main()
 {
-    GothEngine::math::vector::vec2_t vec{1, 2};
-
-    std::cout << vec.x() << ", " << vec.y() << std::endl;
     return 0;
 }
