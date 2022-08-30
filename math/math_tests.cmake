829809b0725986d29b9f8a2a0178537077f61384
Islam <islam_tramov@mail.ru>
2022-08-30 09:20:29 +0300

    Set bin dir for tests

--- math/math_tests.cmake
+++ math/math_tests.cmake
@@ -2,6 +2,11 @@ add_executable(math_tests tests.cpp)
 target_link_libraries(math_tests 
                         PRIVATE  
                         math
-                        GTest::gtest 
+                        GTest::gtest
                         GTest::gtest_main
                         )
+
+set_target_properties(math_tests
+    PROPERTIES
+    RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin"
+)
